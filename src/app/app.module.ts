600dfdb2bfaa7687362807bd2fc14551fd40810a
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-28 08:15:55 -0700

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { WeatherDisplayComponent } from './weather-display/weather-display.component';
+import { WeatherService } from './weather.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherCardComponent,
+    WeatherDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
